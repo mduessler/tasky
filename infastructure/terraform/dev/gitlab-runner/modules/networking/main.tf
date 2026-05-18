8bf40a38c2b6baa5945b084690acc71d336c5c8d
Max Duessler <max.duessler@posteo.net>
2026-05-18 18:29:09 +0200

    feat: Added private and public aws_subnet.

--- infastructure/terraform/dev/gitlab-runner/modules/networking/main.tf
+++ infastructure/terraform/dev/gitlab-runner/modules/networking/main.tf
@@ -3,3 +3,17 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 }
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = false
+}
