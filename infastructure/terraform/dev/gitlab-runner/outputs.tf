9494d720d4b4d0a5f99d71fdf7df3917fdf2c947
Max Duessler <max.duessler@posteo.net>
2026-05-18 19:53:11 +0200

    feat: Exposing security group.

--- infastructure/terraform/dev/gitlab-runner/outputs.tf
+++ infastructure/terraform/dev/gitlab-runner/outputs.tf
@@ -7,3 +7,8 @@ output "subnet_id" {
   description = "ID of the private subnet"
   value       = module.networking.subnet_id
 }
+
+output "runner_security_group_id" {
+  description = "ID of the runner security group"
+  value       = module.networking.runner_security_group_id
+}
