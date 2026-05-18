0dc0f683bb360ec7db27143fd642ab8ebba150b5
Max Duessler <max.duessler@posteo.net>
2026-05-18 19:31:12 +0200

    feat: Added route tables for private and public.

--- infastructure/terraform/dev/gitlab-runner/modules/networking/main.tf
+++ infastructure/terraform/dev/gitlab-runner/modules/networking/main.tf
@@ -32,3 +32,21 @@ resource "aws_nat_gateway" "main" {
   subnet_id     = aws_subnet.public.id
   depends_on    = [aws_internet_gateway.main]
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+}
