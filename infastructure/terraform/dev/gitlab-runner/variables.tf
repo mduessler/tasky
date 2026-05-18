482254b9501d34f92feb5e982f3b2c613b6d5606
Max Duessler <max.duessler@posteo.net>
2026-05-18 17:44:09 +0200

    feat: Added declaration of environment.

--- infastructure/terraform/dev/gitlab-runner/variables.tf
+++ infastructure/terraform/dev/gitlab-runner/variables.tf
@@ -2,3 +2,9 @@ variable "aws_region" {
   description = "AWS Region where all resources will be deployed"
   type        = string
 }
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string  # "dev", "staging", "prod"
+}
+
