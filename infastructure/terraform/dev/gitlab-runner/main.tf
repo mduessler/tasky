c547ef5eabe4b75a63b25a19a9c51690c01b064e
Max Duessler <max.duessler@posteo.net>
2026-05-18 20:07:56 +0200

    feat: Export iam module.

--- infastructure/terraform/dev/gitlab-runner/main.tf
+++ infastructure/terraform/dev/gitlab-runner/main.tf
@@ -3,3 +3,8 @@ module "networking" {
   aws_region  = var.aws_region
   environment = var.environment
 }
+
+module "iam" {
+  source      = "./modules/iam"
+  environment = var.environment
+}
--- infastructure/terraform/dev/gitlab-runner/outputs.tf
+++ infastructure/terraform/dev/gitlab-runner/outputs.tf
@@ -12,3 +12,8 @@ output "runner_security_group_id" {
   description = "ID of the runner security group"
   value       = module.networking.runner_security_group_id
 }
+
+output "instance_profile_name" {
+  description = "IAM instance profile name for the runner"
+  value       = module.iam.instance_profile_name
+}
