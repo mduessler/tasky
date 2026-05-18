7adcc149970ae79300a6b0723241606831dd4db5
Max Duessler <max.duessler@posteo.net>
2026-05-18 19:34:07 +0200

    feat: Added bind for private and public subnet.

--- infastructure/terraform/dev/gitlab-runner/modules/networking/main.tf
+++ infastructure/terraform/dev/gitlab-runner/modules/networking/main.tf
@@ -50,3 +50,13 @@ resource "aws_route_table" "private" {
     nat_gateway_id = aws_nat_gateway.main.id
   }
 }
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
