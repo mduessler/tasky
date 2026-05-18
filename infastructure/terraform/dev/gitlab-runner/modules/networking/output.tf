5236a12a89e2fa45518cf14546c8cd6420d61fe7
Max Duessler <max.duessler@posteo.net>
2026-05-18 18:35:34 +0200

    feat: Added output to networking.

---
+++ infastructure/terraform/dev/gitlab-runner/modules/networking/output.tf
@@ -0,0 +1,9 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  description = "ID of the private subnet"
+  value       = aws_subnet.private.id
+}
