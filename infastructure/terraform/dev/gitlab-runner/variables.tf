52350e9a735ba35d44b8d327a98469f0365c800e
Max Duessler <max.duessler@posteo.net>
2026-05-19 03:39:42 +0200

    feat: Declared variables for configuration.

--- infastructure/terraform/dev/gitlab-runner/variables.tf
+++ infastructure/terraform/dev/gitlab-runner/variables.tf
@@ -7,3 +7,15 @@ variable "environment" {
   description = "Deployment environment"
   type        = string
 }
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "gitlab_runner_token" {
+  description = "GitLab Runner registration token"
+  type        = string
+  sensitive   = true
+}
