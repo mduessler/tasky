5740a0482f9c6e859b1c725b8b00ffb56a997777
Max Duessler <max.duessler@posteo.net>
2026-05-19 10:08:43 +0200

    feat: Added security group for https only, and only outgoing traffic.

--- infastructure/terraform/environment/dev/gitlab-runner/modules/network/maint.tf
+++ infastructure/terraform/environment/dev/gitlab-runner/modules/network/maint.tf
@@ -65,3 +65,20 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+#
+# Security group (HTTPS only, only outgoind)
+#
+
+resource "aws_security_group" "runner" {
+  name        = "gitlab-runner-sg"
+  description = "Security Group for GitLab Runner - outbound only"
+  vpc_id      = aws_vpc.main.id
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
