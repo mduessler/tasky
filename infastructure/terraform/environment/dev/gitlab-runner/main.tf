8f13be8402b2c488c2bcdd765c8e60d5b1b4e7a3
Max Duessler <max.duessler@posteo.net>
2026-05-19 15:34:23 +0200

    fix: Name error.

--- infastructure/terraform/environment/dev/gitlab-runner/main.tf
+++ infastructure/terraform/environment/dev/gitlab-runner/main.tf
@@ -10,10 +10,10 @@ module "security" {
 }
 
 module "compute" {
-  source                   = "./modules/compute"
-  environment              = var.environment
-  instance_type            = var.instance_type
-  private_subnet_id        = module.networking.private_subnet_id
-  runner_security_group_id = module.networking.security_group_id
-  instance_profile_name    = module.security.instance_profile_name
+  source                = "./modules/compute"
+  environment           = var.environment
+  instance_type         = var.instance_type
+  subnet_id             = module.networking.private_subnet_id
+  runner_security_group = module.networking.security_group_id
+  permission_profile = module.security.instance_profile_name
 }
