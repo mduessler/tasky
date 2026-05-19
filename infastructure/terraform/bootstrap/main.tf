09ea827d7724433ccce0ccef0e4fb9ac28831412
Max Duessler <max.duessler@posteo.net>
2026-05-19 05:51:42 +0200

    refactor: Importing security state from module.

--- infastructure/terraform/bootstrap/main.tf
+++ infastructure/terraform/bootstrap/main.tf
@@ -13,25 +13,6 @@ resource "aws_s3_bucket_versioning" "terraform_state" {
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "gitlab-runner-terraform-locks"
   billing_mode = "PAY_PER_REQUEST"
@@ -64,4 +45,9 @@ resource "aws_s3_bucket_policy" "terraform_state" {
       }
     }]
   })
+}
+
+module "s3_security_state" {
+  source    = "./modules/s3_security"
+  bucket_id = aws_s3_bucket.terraform_state.id
 }
