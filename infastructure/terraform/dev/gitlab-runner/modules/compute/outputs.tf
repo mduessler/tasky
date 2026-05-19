41b09bd04cd6f2094224cba97eccf67cde32958c
Max Duessler <max.duessler@posteo.net>
2026-05-19 04:58:34 +0200

    chore: Moved dev to end

--- infastructure/terraform/dev/gitlab-runner/modules/compute/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "runner_instance_id" {
-  description = "ID of the GitLab Runner EC2 instance"
-  value       = aws_instance.runner.id
-}
--- infastructure/terraform/dev/gitlab-runner/modules/iam/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "instance_profile_name" {
-  description = "IAM instance profile name for the runner"
-  value       = aws_iam_instance_profile.runner.name
-}
