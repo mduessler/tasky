4cd9f1fdcf5e1850532dca471621381b08ddc4fd
Max Duessler <max.duessler@posteo.net>
2026-05-19 04:41:11 +0200

    feat: Added encryption, to secure sensitive data.

--- infastructure/terraform/dev/bootstrap-backend/main.tf
+++ infastructure/terraform/dev/bootstrap-backend/main.tf
@@ -9,3 +9,13 @@ resource "aws_s3_bucket_versioning" "terraform_state" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
