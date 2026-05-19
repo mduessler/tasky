1c9b585e6ad7cf67cb70811cf7f946085ba1aa49
Max Duessler <max.duessler@posteo.net>
2026-05-19 16:39:37 +0200

    feat: Added dynamically policies to role to IAM user

--- infastructure/terraform/environment/dev/gitlab-runner/modules/security/main.tf
+++ infastructure/terraform/environment/dev/gitlab-runner/modules/security/main.tf
@@ -15,3 +15,20 @@ resource "aws_iam_instance_profile" "gitlab_runner" {
   name = "gitlab-runner-profile-${var.environment}"
   role = aws_iam_role.gitlab_runner.name
 }
+
+locals {
+  runner_policies = [
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
+    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
+    "arn:aws:iam::aws:policy/IAMFullAccess",
+  ]
+}
+
+resource "aws_iam_role_policy_attachment" "runner" {
+  for_each   = toset(local.runner_policies)
+  role       = aws_iam_role.gitlab_runner.name
+  policy_arn = each.value
+}
