eb9288f067e94f10c867ed4406efc56a1221bd75
Max Duessler <max.duessler@posteo.net>
2026-05-19 03:36:52 +0200

    refactor: Added newlines between variables.

--- infastructure/terraform/dev/gitlab-runner/modules/networking/variables.tf
+++ infastructure/terraform/dev/gitlab-runner/modules/networking/variables.tf
@@ -3,10 +3,12 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
 variable "aws_region" {
   description = "AWS Region"
   type        = string
 }
+
 variable "environment" {
   description = "Deployment environment"
   type        = string
