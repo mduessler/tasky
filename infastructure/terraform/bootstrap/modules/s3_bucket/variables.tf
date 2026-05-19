b1714c7c7060f114e6873d31c05195fa05c7178f
Max Duessler <max.duessler@posteo.net>
2026-05-19 14:43:51 +0200

    fix: Removed wrong variable.

--- infastructure/terraform/bootstrap/modules/s3_bucket/variables.tf
+++ infastructure/terraform/bootstrap/modules/s3_bucket/variables.tf
@@ -3,7 +3,7 @@ variable "name" {
   type        = string
 }
 
-variable "prevent_destroy" {
-  description = "Enable or disable deletion of bucket, if it is new initialized."
-  type        = bool
+variable "version_status" {
+  description = "Set the status of the versioning"
+  type        = string
 }
