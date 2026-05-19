eb6daa3d0f109b83197324801b16ebad59eec173
Max Duessler <max.duessler@posteo.net>
2026-05-19 05:04:11 +0200

    feat: Added https only security resource.

--- infastructure/terraform/bootstrap/main.tf
+++ infastructure/terraform/bootstrap/main.tf
@@ -39,3 +39,26 @@ resource "aws_dynamodb_table" "terraform_locks" {
     type = "S"
   }
 }
+
+resource "aws_s3_bucket_policy" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  depends_on = [aws_s3_bucket_public_access_block.terraform_state]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "DenyNonTLS"
+      Effect    = "Deny"
+      Principal = "*"
+      Action    = "s3:*"
+      Resource = [
+        aws_s3_bucket.terraform_state.arn,
+        "${aws_s3_bucket.terraform_state.arn}/*"
+      ]
+      Condition = {
+        Bool = { "aws:SecureTransport" = "false" }
+      }
+    }]
+  })
+}
