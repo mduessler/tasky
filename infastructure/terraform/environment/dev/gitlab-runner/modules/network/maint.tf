c1264aa4450d7d7b86d5d703fd0e0bd8945f1064
Max Duessler <max.duessler@posteo.net>
2026-05-19 09:52:33 +0200

    feat: Added components to run a private subnet.

--- infastructure/terraform/environment/dev/gitlab-runner/modules/network/maint.tf
+++ infastructure/terraform/environment/dev/gitlab-runner/modules/network/maint.tf
@@ -3,3 +3,27 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 }
+
+#
+# Private subnets components
+#
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
