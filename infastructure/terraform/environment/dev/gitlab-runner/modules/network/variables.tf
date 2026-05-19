d79cd098e47698e6d41d5c2342cf630c9a921ff6
Max Duessler <max.duessler@posteo.net>
2026-05-19 09:50:38 +0200

    feat: Added private subnet in which gitlab-runner runs.

--- infastructure/terraform/environment/dev/gitlab-runner/modules/network/variables.tf
+++ infastructure/terraform/environment/dev/gitlab-runner/modules/network/variables.tf
@@ -3,3 +3,8 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "aws_region" {
+  description = "AWS Region where all resources will be deployed"
+  type        = string
+}
