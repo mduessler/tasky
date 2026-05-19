75369c950de1d8faf0e9250d6240b29c6dad8cf8
Max Duessler <max.duessler@posteo.net>
2026-05-19 04:34:33 +0200

    feat: Added versioning. For rollbacks, etc ...

--- infastructure/terraform/dev/bootstrap-backend/main.tf
+++ infastructure/terraform/dev/bootstrap-backend/main.tf
@@ -1,3 +1,11 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "gitlab-runner-terraform-state-REDACTED_AWS_ACCOUNT"
 }
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
