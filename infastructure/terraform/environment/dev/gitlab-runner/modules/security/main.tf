40409cc25fae68ba5973327d9c615ecb2f41c2db
Max Duessler <max.duessler@posteo.net>
2026-05-19 09:18:32 +0200

    feat: Added permission role for gitlab-runner

---
+++ infastructure/terraform/environment/dev/gitlab-runner/modules/security/main.tf
@@ -0,0 +1,12 @@
+resource "aws_iam_role" "gitlab_runner" {
+  name = "gitlab-runner-role-${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
