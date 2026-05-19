0f7969d95b04700d260f28491f3cb7b94e4db547
Max Duessler <max.duessler@posteo.net>
2026-05-19 04:42:43 +0200

    feat: Added table to store locks.

--- infastructure/terraform/dev/bootstrap-backend/main.tf
+++ infastructure/terraform/dev/bootstrap-backend/main.tf
@@ -28,3 +28,14 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "gitlab-runner-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
