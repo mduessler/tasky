d5dd5719fac73cd7d012bf6a8031f7d6ca12ece7
Max Duessler <max.duessler@posteo.net>
2026-05-19 10:01:41 +0200

    feat: Added public subnet.

--- infastructure/terraform/environment/dev/gitlab-runner/modules/network/maint.tf
+++ infastructure/terraform/environment/dev/gitlab-runner/modules/network/maint.tf
@@ -27,3 +27,27 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
 }
+
+#
+# Public subnet components
+#
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
