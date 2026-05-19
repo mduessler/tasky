82b71f4273744a6e3aa6767f49919180b7ac6bed
Max Duessler <max.duessler@posteo.net>
2026-05-19 03:38:09 +0200

    feat: Importing compute module.

--- infastructure/terraform/dev/gitlab-runner/main.tf
+++ infastructure/terraform/dev/gitlab-runner/main.tf
@@ -8,3 +8,13 @@ module "iam" {
   source      = "./modules/iam"
   environment = var.environment
 }
+
+module "compute" {
+  source                   = "./modules/compute"
+  environment              = var.environment
+  instance_type            = var.instance_type
+  private_subnet_id        = module.networking.private_subnet_id
+  runner_security_group_id = module.networking.runner_security_group_id
+  instance_profile_name    = module.iam.instance_profile_name
+  gitlab_runner_token      = var.gitlab_runner_token
+}
