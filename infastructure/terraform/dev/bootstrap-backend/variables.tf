8f5235ce11e3b2add9b80337dd59edeb13575451
Max Duessler <max.duessler@posteo.net>
2026-05-19 04:25:54 +0200

    feat: Declared variables for backend.

---
+++ infastructure/terraform/dev/bootstrap-backend/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS Region where all resources will be deployed"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
