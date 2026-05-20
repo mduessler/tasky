098cf8868f80536f8a617ff01b8fbf3ad4fa4f61
Max Duessler <max.duessler@posteo.net>
2026-05-20 10:43:12 +0200

    refactor: formated code.

--- infastructure/terraform/environment/dev/gitlab-runner/main.tf
+++ infastructure/terraform/environment/dev/gitlab-runner/main.tf
@@ -1,6 +1,6 @@
 module "network" {
-  source      = "./modules/network"
-  aws_region  = var.aws_region
+  source     = "./modules/network"
+  aws_region = var.aws_region
 }
 
 module "security" {
@@ -14,5 +14,5 @@ module "compute" {
   instance_type         = var.instance_type
   subnet_id             = module.network.private_subnet_id
   runner_security_group = module.network.security_group_id
-  permission_profile = module.security.instance_profile_name
+  permission_profile    = module.security.instance_profile_name
 }
