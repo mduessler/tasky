319af43b789444ccfb9cbb178543c86b4a1bf2df
Max Duessler <max.duessler@posteo.net>
2026-05-20 07:37:49 +0200

    refactor: Changed project name, for better identification.

--- infastructure/terraform/environment/dev/gitlab-runner/provider.tf
+++ infastructure/terraform/environment/dev/gitlab-runner/provider.tf
@@ -20,7 +20,7 @@ provider "aws" {
 
   default_tags {
     tags = {
-      Project     = "tasky"
+      Project     = "tasky-gitlab-runner"
       Environment = var.environment
       ManagedBy   = "terraform"
     }
