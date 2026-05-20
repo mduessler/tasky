7e5a077f001666f8b4b81f3a646a790e6c1e504c
Max Duessler <max.duessler@posteo.net>
2026-05-20 09:02:12 +0200

    feat: Exporting user for other modules.

--- infastructure/terraform/bootstrap/output.tf
+++ infastructure/terraform/bootstrap/output.tf
@@ -5,3 +5,16 @@ output "s3_bucket_name" {
 output "dynamodb_table_name" {
   value = aws_dynamodb_table.terraform_locks.name
 }
+
+output "access_key_id" {
+  value = aws_iam_access_key.this.id
+}
+
+output "secret_access_key" {
+  value     = aws_iam_access_key.this.secret
+  sensitive = true
+}
+
+output "user_name" {
+  value = aws_iam_user.this.name
+}
