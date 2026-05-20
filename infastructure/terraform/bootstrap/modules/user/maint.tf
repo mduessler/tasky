af09c6c17a6dd76a0a54786fe920e239927c0d88
Max Duessler <max.duessler@posteo.net>
2026-05-20 08:59:00 +0200

    refactor: Changed name.

--- infastructure/terraform/bootstrap/modules/user/maint.tf
+++ infastructure/terraform/bootstrap/modules/user/maint.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_user" "gitlab_runner" {
+resource "aws_iam_user" "this" {
   name = var.username
 
   tags = {
@@ -9,16 +9,16 @@ resource "aws_iam_user" "gitlab_runner" {
 
 }
 
-resource "aws_iam_access_key" "gitlab_runner" {
-  user = aws_iam_user.gitlab_runner.name
+resource "aws_iam_access_key" "this" {
+  user = aws_iam_user.this.name
 }
 
 resource "aws_iam_user_policy_attachment" "s3_access" {
-  user       = aws_iam_user.gitlab_runner.name
+  user       = aws_iam_user.this.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
 resource "aws_iam_user_policy_attachment" "dynamodb_access" {
-  user       = aws_iam_user.gitlab_runner.name
+  user       = aws_iam_user.this.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
--- infastructure/terraform/bootstrap/modules/user/output.tf
+++ infastructure/terraform/bootstrap/modules/user/output.tf
@@ -1,8 +1,8 @@
 output "access_key_id" {
-  value = aws_iam_access_key.gitlab_runner.id
+  value = aws_iam_access_key.this.id
 }
 
 output "secret_access_key" {
-  value     = aws_iam_access_key.gitlab_runner.secret
+  value     = aws_iam_access_key.this.secret
   sensitive = true
 }
