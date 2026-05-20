f9c1aa013b9b479dbdb19b2dbad21f4d78e34bbf
Max Duessler <max.duessler@posteo.net>
2026-05-20 09:46:55 +0200

    Adapted output to new terraform

--- infastructure/terraform/bootstrap/output.tf
+++ infastructure/terraform/bootstrap/output.tf
@@ -1,20 +1,20 @@
-output "s3_bucket_name" {
-  value = module.state_bucket.id
+output "s3_bucket_name_dev" {
+  value = module.state_bucket_dev.id
 }
 
-output "dynamodb_table_name" {
-  value = aws_dynamodb_table.terraform_locks.name
+output "dynamodb_table_name_dev" {
+  value = module.terraform_locks_dev.table_name
 }
 
-output "access_key_id" {
-  value = aws_iam_access_key.this.id
+output "access_key_id_dev" {
+  value = module.tasky_gitlab_runner_user_dev.access_key_id
 }
 
-output "secret_access_key" {
-  value     = aws_iam_access_key.this.secret
+output "secret_access_key_dev" {
+  value     = module.tasky_gitlab_runner_user_dev.secret_access_key
   sensitive = true
 }
 
-output "user_name" {
-  value = aws_iam_user.this.name
+output "user_name_dev" {
+  value = module.tasky_gitlab_runner_user_dev.user_name
 }
