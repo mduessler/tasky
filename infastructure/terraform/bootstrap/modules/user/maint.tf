1a232d2d431168f2cb10f090e729c512bda73624
Max Duessler <max.duessler@posteo.net>
2026-05-20 08:47:21 +0200

    feat: Giving user access to bucket and lock database.

--- infastructure/terraform/bootstrap/modules/user/maint.tf
+++ infastructure/terraform/bootstrap/modules/user/maint.tf
@@ -5,3 +5,13 @@ resource "aws_iam_user" "gitlab_runner" {
 resource "aws_iam_access_key" "gitlab_runner" {
   user = aws_iam_user.gitlab_runner.name
 }
+
+resource "aws_iam_user_policy_attachment" "s3_access" {
+  user       = aws_iam_user.gitlab_runner.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_user_policy_attachment" "dynamodb_access" {
+  user       = aws_iam_user.gitlab_runner.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
