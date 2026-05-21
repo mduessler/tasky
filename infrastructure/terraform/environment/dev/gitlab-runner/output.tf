b9048db580c34a3808447e6bbef6e225d1c285ea
Max Duessler <max.duessler@posteo.net>
2026-05-21 08:18:41 +0200

    feat: Added output, to get copy paste connection to runner server.

--- infrastructure/terraform/environment/dev/gitlab-runner/output.tf
+++ infrastructure/terraform/environment/dev/gitlab-runner/output.tf
@@ -2,3 +2,12 @@ output "gitlab_runner_instance_id" {
   description = "Instance ID des GitLab Runners (für SSM)"
   value       = module.compute.gitlab_runner
 }
+
+output "ssm_bucket_name" {
+  value = module.ssm_transfer_bucket.id
+}
+
+output "ssm_session_command" {
+  description = "Instruction how to connect via SSM."
+  value       = "aws ssm start-session --target ${module.compute.gitlab_runner} --region ${var.aws_region} --profile tasky-dev"
+}
