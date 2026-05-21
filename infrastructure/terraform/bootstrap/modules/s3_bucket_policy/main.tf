8a947b9196c35c22cb767d95d8a5946c2bc358ca
Max Duessler <max.duessler@posteo.net>
2026-05-21 06:55:40 +0200

    refactor: Delete unused module.

--- infrastructure/terraform/bootstrap/modules/s3_bucket_policy/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket_policy" "this" {
-  bucket = var.bucket_id
-
-  policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = var.policy_statements
-  })
-}
--- infrastructure/terraform/bootstrap/modules/s3_bucket_policy/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_id" {
-  description = "S3 Bucket ID"
-  type        = string
-}
-
-variable "policy_statements" {
-  description = "List of policy statements"
-  type        = any
-}
