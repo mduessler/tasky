b95b9290eefe01f02df66fceaaa10ecca04f20b6
Max Duessler <max.duessler@posteo.net>
2026-05-21 07:21:39 +0200

    chore: New module for reusable code.

---
+++ infrastructure/terraform/modules/s3_bucket/variables.tf
@@ -0,0 +1,16 @@
+variable "name" {
+  description = "Unique Bucket name."
+  type        = string
+}
+
+variable "version_status" {
+  description = "Set the status of the versioning"
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  description = "Resource-level tags merged with the provider's default_tags."
+  type        = map(string)
+  default     = {}
+}
---
+++ infrastructure/terraform/modules/s3_security/variables.tf
@@ -0,0 +1,4 @@
+variable "bucket_id" {
+  description = "The ID of the S3 bucket to apply security configuration to"
+  type        = string
+}
