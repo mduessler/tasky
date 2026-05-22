6725b130b920b3c4b66f52076535268f299b1d00
Max Duessler <max.duessler@posteo.net>
2026-05-22 15:09:56 +0200

    fix: Disabled http

--- infrastructure/terraform/environment/dev/gitlab-runner/modules/network/main.tf
+++ infrastructure/terraform/environment/dev/gitlab-runner/modules/network/main.tf
@@ -76,13 +76,6 @@ resource "aws_security_group" "runner" {
   vpc_id      = aws_vpc.main.id
 
   egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
