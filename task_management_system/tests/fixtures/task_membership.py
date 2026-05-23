b3cdead1f3c63730b57ce04ed604376b7224cd23
Max Duessler <max.duessler@posteo.net>
2026-05-23 17:46:12 +0200

    test: Created read only fixture

--- task_management_system/tests/fixtures/task_membership.py
+++ task_management_system/tests/fixtures/task_membership.py
@@ -59,3 +59,76 @@ def superuser_is_not_member(db, task, five_users):
 @pytest.fixture
 def user_is_not_member(db, five_users):
     return five_users[4], None
+
+
+@pytest.fixture(scope="class")
+def task_memberships_read_only(
+    django_db_setup, django_db_blocker, task_read_only, users_read_only
+):
+    with django_db_blocker.unblock():
+        TaskMembershipFactory.create_batch(5, task=task_read_only)
+
+
+@pytest.fixture(scope="class")
+def task_membership_read_only(django_db_setup, django_db_blocker, task_read_only, users_read_only):
+    with django_db_blocker.unblock():
+        return TaskMembershipFactory(task=task_read_only)
+
+
+@pytest.fixture(scope="class")
+def five_users_read_only(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        return UserFactory.create_batch(5)
+
+
+@pytest.fixture(scope="class")
+def member_is_owner_read_only(
+    django_db_setup, django_db_blocker, task_read_only, five_users_read_only
+):
+    with django_db_blocker.unblock():
+        return five_users_read_only[0], TaskMembershipFactory(
+            user=five_users_read_only[0], task=task_read_only, role=Role.OWNER
+        )
+
+
+@pytest.fixture(scope="class")
+def member_is_admin_read_only(
+    django_db_setup, django_db_blocker, task_read_only, five_users_read_only
+):
+    with django_db_blocker.unblock():
+        return five_users_read_only[1], TaskMembershipFactory(
+            user=five_users_read_only[1], task=task_read_only, role=Role.ADMIN
+        )
+
+
+@pytest.fixture(scope="class")
+def member_is_member_read_only(
+    django_db_setup, django_db_blocker, task_read_only, five_users_read_only
+):
+    with django_db_blocker.unblock():
+        return five_users_read_only[2], TaskMembershipFactory(
+            user=five_users_read_only[2], task=task_read_only, role=Role.MEMBER
+        )
+
+
+@pytest.fixture(scope="class")
+def member_is_viewer_read_only(
+    django_db_setup, django_db_blocker, task_read_only, five_users_read_only
+):
+    with django_db_blocker.unblock():
+        return five_users_read_only[3], TaskMembershipFactory(
+            user=five_users_read_only[3], task=task_read_only, role=Role.VIEWER
+        )
+
+
+@pytest.fixture(scope="class")
+def superuser_is_not_member_read_only(
+    django_db_setup, django_db_blocker, task_read_only, five_users_read_only
+):
+    with django_db_blocker.unblock():
+        return SuperuserFactory(), None
+
+
+@pytest.fixture(scope="class")
+def user_is_not_member_read_only(five_users_read_only):
+    return five_users_read_only[4], None
--- task_management_system/tests/fixtures/user.py
+++ task_management_system/tests/fixtures/user.py
@@ -83,3 +83,68 @@ def superuser(db, superuser_data):
 @pytest.fixture
 def anonymous_user():
     return AnonymousUser()
+
+
+@pytest.fixture(scope="class")
+def users_read_only(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        UserFactory.create_batch(20)
+
+
+@pytest.fixture(scope="class")
+def active_user_data_read_only():
+    return {
+        "password": "ThisIsATestingPassword",
+        "username": "ZockerHeld99",
+        "first_name": "Max",
+        "last_name": "Mustermann",
+        "email": "max@example.com",
+        "is_active": True,
+    }
+
+
+@pytest.fixture(scope="class")
+def inactive_user_data_read_only():
+    return {
+        "password": "ThisIsATestingPassword",
+        "username": "NoobMaster",
+        "first_name": "Kevin",
+        "last_name": "Müller",
+        "email": "kevin@example.com",
+        "is_active": False,
+    }
+
+
+@pytest.fixture(scope="class")
+def superuser_data_read_only():
+    return {
+        "password": "ThisIsATestingPassword",
+        "username": "PixelQueen",
+        "first_name": "Sarah",
+        "last_name": "Schmidt",
+        "email": "sara@example.com",
+        "is_active": True,
+    }
+
+
+@pytest.fixture(scope="class")
+def active_user_read_only(django_db_setup, django_db_blocker, active_user_data_read_only):
+    with django_db_blocker.unblock():
+        return UserFactory(**active_user_data_read_only)
+
+
+@pytest.fixture(scope="class")
+def inactive_user_read_only(django_db_setup, django_db_blocker, inactive_user_data_read_only):
+    with django_db_blocker.unblock():
+        return UserFactory(**inactive_user_data_read_only)
+
+
+@pytest.fixture(scope="class")
+def superuser_read_only(django_db_setup, django_db_blocker, superuser_data_read_only):
+    with django_db_blocker.unblock():
+        return SuperuserFactory(**superuser_data_read_only)
+
+
+@pytest.fixture(scope="class")
+def anonymous_user_read_only():
+    return AnonymousUser()
