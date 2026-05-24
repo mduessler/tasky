0b3e6f5043c545f752b360daccc3b8979d4a4151
Max Duessler <max.duessler@posteo.net>
2026-05-24 09:56:04 +0200

    test: Added read only

--- task_management_system/tests/user/api/v1/views/test_tms_user_view_validation_error.py
+++ task_management_system/tests/user/api/v1/views/test_tms_user_view_validation_error.py
@@ -8,17 +8,17 @@ BASENAME = "user"
 
 @pytest.mark.django_db
 class TestValidationError:
-    def test_patch(self, superuser, active_user, api_client):
-        old_username = active_user.username
+    def test_patch(self, superuser_read_only, active_user_read_only, api_client):
+        old_username = active_user_read_only.username
 
         data = {"username": ""}
-        url = parse_url(BASENAME, "detail", active_user.id)
+        url = parse_url(BASENAME, "detail", active_user_read_only.id)
 
-        api_client.force_authenticate(user=superuser)
+        api_client.force_authenticate(user=superuser_read_only)
         response = api_client.patch(url, data=data)
 
-        active_user.refresh_from_db()
+        active_user_read_only.refresh_from_db()
         assert response.status_code == 400
         assert set(response.data.keys()) == {"username"}
         assert str(response.data["username"][0]) == "This field may not be blank."
-        assert active_user.username == old_username
+        assert active_user_read_only.username == old_username
