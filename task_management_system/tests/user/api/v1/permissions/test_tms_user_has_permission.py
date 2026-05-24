cb015b953a33f4905d6b5a6bafc6e3cb416d91bf
Max Duessler <max.duessler@posteo.net>
2026-05-24 10:15:19 +0200

    test: Added preload

--- task_management_system/tests/user/api/v1/permissions/test_tms_user_has_permission.py
+++ task_management_system/tests/user/api/v1/permissions/test_tms_user_has_permission.py
@@ -5,6 +5,18 @@ from user.api.v1.permissions import TmsUserPermission
 
 @pytest.mark.django_db
 class TestHasPermission:
+    @pytest.fixture(scope="class", autouse=True)
+    def preload(
+        self,
+        member_is_owner_read_only,
+        member_is_admin_read_only,
+        member_is_member_read_only,
+        member_is_viewer_read_only,
+        user_is_not_member_read_only,
+        superuser_read_only,
+    ):
+        pass
+
     def test_action_options_allowed(self, superuser_read_only, caplog_loguru):
         req = build_request("options", superuser_read_only)
         result = TmsUserPermission().has_permission(req, DummyView("options"))
