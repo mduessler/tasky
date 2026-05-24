cc2140d95ddc02b408cdebcfbd8c54caa920b552
Max Duessler <max.duessler@posteo.net>
2026-05-24 07:19:52 +0200

    test: Using read only fixtures

--- task_management_system/tests/task/models/test_task_membership.py
+++ task_management_system/tests/task/models/test_task_membership.py
@@ -7,42 +7,67 @@ from task.models import Role, TaskMembership
 @pytest.mark.django_db
 class TestTaskMembership:
 
-    def test_create_membership_success(self, task, active_user):
-        membership = TaskMembership.objects.create(user=active_user, task=task, role=Role.MEMBER)
+    def test_create_membership_success(self, task_read_only, active_user_read_only):
+        membership = TaskMembership.objects.create(
+            user=active_user_read_only, task=task_read_only, role=Role.MEMBER
+        )
         assert membership.role == Role.MEMBER
-        assert str(membership) == f"{active_user.pk} - {task.pk} (member)"
+        assert str(membership) == f"{active_user_read_only.pk} - {task_read_only.pk} (member)"
 
-    def test_unique_user_task_constraint(self, task, active_user):
-        TaskMembership.objects.create(user=active_user, task=task, role=Role.MEMBER)
+    def test_unique_user_task_constraint(self, task_read_only, active_user_read_only):
+        TaskMembership.objects.create(
+            user=active_user_read_only, task=task_read_only, role=Role.MEMBER
+        )
 
         with pytest.raises(ValidationError):
-            TaskMembership.objects.create(user=active_user, task=task, role=Role.VIEWER)
+            TaskMembership.objects.create(
+                user=active_user_read_only, task=task_read_only, role=Role.VIEWER
+            )
 
-    def test_only_one_owner_per_task(self, task, active_user):
+    def test_only_one_owner_per_task(self, task_read_only, active_user_read_only):
+        TaskMembership.objects.create(
+            user=active_user_read_only, task=task_read_only, role=Role.OWNER
+        )
         with pytest.raises(ValidationError):
-            TaskMembership.objects.create(user=active_user, task=task, role=Role.OWNER)
+            TaskMembership.objects.create(
+                user=active_user_read_only, task=task_read_only, role=Role.OWNER
+            )
 
-    def test_multiple_members_allowed(self, task, active_user, superuser):
-        cnt = TaskMembership.objects.count()
-        TaskMembership.objects.create(user=active_user, task=task, role=Role.MEMBER)
-        TaskMembership.objects.create(user=superuser, task=task, role=Role.MEMBER)
+    def test_multiple_members_allowed(
+        self, task_read_only, active_user_read_only, superuser_read_only
+    ):
+        cnt = TaskMembership.objects.filter(task=task_read_only).count()
+        TaskMembership.objects.create(
+            user=active_user_read_only, task=task_read_only, role=Role.MEMBER
+        )
+        TaskMembership.objects.create(
+            user=superuser_read_only, task=task_read_only, role=Role.MEMBER
+        )
 
-        assert TaskMembership.objects.filter(task=task).count() == cnt + 2
+        assert TaskMembership.objects.filter(task=task_read_only).count() == cnt + 2
 
-    def test_invalid_role_raises_error(self, task, active_user):
+    def test_invalid_role_raises_error(self, task_read_only, active_user_read_only):
         with pytest.raises(ValidationError) as excinfo:
-            TaskMembership.objects.create(user=active_user, task=task, role="GHT")
+            TaskMembership.objects.create(
+                user=active_user_read_only, task=task_read_only, role="GHT"
+            )
         assert "role" in excinfo.value.message_dict
 
-    def test_get_user_role_utility(self, task, active_user):
-        TaskMembership.objects.create(user=active_user, task=task, role=Role.ADMIN)
+    def test_get_user_role_utility(self, task_read_only, active_user_read_only):
+        TaskMembership.objects.create(
+            user=active_user_read_only, task=task_read_only, role=Role.ADMIN
+        )
 
-        assert TaskMembership.get_user_role(active_user, task) == Role.ADMIN
-        assert TaskMembership.get_user_role(None, task) is None
+        assert TaskMembership.get_user_role(active_user_read_only, task_read_only) == Role.ADMIN
+        assert TaskMembership.get_user_role(None, task_read_only) is None
 
-    def test_integrity_error_on_db_level(self, task, active_user):
-        TaskMembership.objects.create(user=active_user, task=task, role=Role.MEMBER)
-        duplicate_membership = TaskMembership(user=active_user, task=task, role=Role.VIEWER)
+    def test_integrity_error_on_db_level(self, task_read_only, active_user_read_only):
+        TaskMembership.objects.create(
+            user=active_user_read_only, task=task_read_only, role=Role.MEMBER
+        )
+        duplicate_membership = TaskMembership(
+            user=active_user_read_only, task=task_read_only, role=Role.VIEWER
+        )
 
         with pytest.raises(IntegrityError):
             duplicate_membership.save(skip_validation=True)
--- task_management_system/tests/task/models/test_task_note.py
+++ task_management_system/tests/task/models/test_task_note.py
@@ -6,50 +6,66 @@ from task.models import Role, TaskMembership, TaskNote
 @pytest.mark.django_db
 class TestTaskNote:
 
-    def test_create_note_success(self, task, active_user):
-        TaskMembership.objects.create(user=active_user, task=task, role=Role.MEMBER)
+    def test_create_note_success(self, task_read_only, active_user_read_only):
+        TaskMembership.objects.create(
+            user=active_user_read_only, task=task_read_only, role=Role.MEMBER
+        )
 
         note_text = "This is an important note"
-        note = TaskNote.objects.create(task=task, author=active_user, note=note_text)
+        note = TaskNote.objects.create(
+            task=task_read_only, author=active_user_read_only, note=note_text
+        )
 
         assert note.pk is not None
         assert note.note == note_text
-        assert note.author == active_user
+        assert note.author == active_user_read_only
 
-    def test_create_note_author_not_member_raises_error(self, task, active_user):
+    def test_create_note_author_not_member_raises_error(
+        self, task_read_only, active_user_read_only
+    ):
         with pytest.raises(ValidationError) as excinfo:
             TaskNote.objects.create(
-                task=task, author=active_user, note="This is an important note"
+                task=task_read_only, author=active_user_read_only, note="This is an important note"
             )
         assert "Author must be a member of the task." in str(excinfo.value)
 
-    def test_create_note_without_author_success(self, task):
-        note = TaskNote.objects.create(task=task, author=None, note="This is an important note")
+    def test_create_note_without_author_success(self, task_read_only):
+        note = TaskNote.objects.create(
+            task=task_read_only, author=None, note="This is an important note"
+        )
         assert note.author is None
         assert note.pk is not None
 
-    def test_note_content_cannot_be_empty(self, task, active_user):
-        TaskMembership.objects.create(user=active_user, task=task, role=Role.MEMBER)
-
-        with pytest.raises(ValidationError):
-            TaskNote.objects.create(task=task, author=active_user, note="")
-
-    def test_on_delete_set_null_author(self, task, active_user):
-        TaskMembership.objects.create(user=active_user, task=task, role=Role.MEMBER)
-        note = TaskNote.objects.create(
-            task=task, author=active_user, note="This is an important note"
+    def test_note_content_cannot_be_empty(self, task_read_only, active_user_read_only):
+        TaskMembership.objects.create(
+            user=active_user_read_only, task=task_read_only, role=Role.MEMBER
         )
 
-        active_user.delete()
+        with pytest.raises(ValidationError):
+            TaskNote.objects.create(task=task_read_only, author=active_user_read_only, note="")
+
+    def test_on_delete_set_null_author(self, task_read_only, active_user_read_only):
+        TaskMembership.objects.create(
+            user=active_user_read_only, task=task_read_only, role=Role.MEMBER
+        )
+        note = TaskNote.objects.create(
+            task=task_read_only, author=active_user_read_only, note="This is an important note"
+        )
+
+        active_user_read_only.delete()
         note.refresh_from_db()
 
         assert note.author is None
         assert note.note == "This is an important note"
 
-    def test_on_delete_cascade_task(self, task, active_user):
-        TaskMembership.objects.create(user=active_user, task=task, role=Role.MEMBER)
-        TaskNote.objects.create(task=task, author=active_user, note="This is an important note")
+    def test_on_delete_cascade_task(self, task_read_only, active_user_read_only):
+        TaskMembership.objects.create(
+            user=active_user_read_only, task=task_read_only, role=Role.MEMBER
+        )
+        TaskNote.objects.create(
+            task=task_read_only, author=active_user_read_only, note="This is an important note"
+        )
 
-        task.delete()
+        task_read_only.delete()
 
         assert TaskNote.objects.count() == 0
