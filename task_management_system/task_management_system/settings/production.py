845b82b50179486ff767a74c6039ac6bb57bc76c
Max Duessler <max.duessler@posteo.net>
2026-05-31 14:27:16 +0200

    feat: Added production settings.

--- task_management_system/task_management_system/settings/production.py
+++ task_management_system/task_management_system/settings/production.py
@@ -1,10 +1,35 @@
+from __future__ import annotations
+
 from os import environ
 
 from task_management_system.core.logging import TmsLogger
 from task_management_system.settings.base import *  # noqa F403
 
 DEBUG = False
+SECRET_KEY = environ["SECRET_KEY"]
+ALLOWED_HOSTS = environ["DJANGO_ALLOWED_HOSTS"].split(" ")
 
-SECRET_KEY = environ.get("SECRET_KEY")
+DATABASES = {
+    "default": {
+        "ENGINE": environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
+        "NAME": environ["SQL_DB"],
+        "USER": environ["SQL_USER"],
+        "PASSWORD": environ["SQL_PASSWORD"],
+        "HOST": environ["SQL_HOST"],
+        "PORT": environ.get("SQL_PORT", "5432"),
+    }
+}
 
-TmsLogger("DEBUG")
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# Secure cookies — only needed, because admin site is still allowed.
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# Nginx handles https checks
+SILENCED_SYSTEM_CHECKS = ["security.W004", "security.W008"]
+
+# We need this because of swagger ui
+STATIC_ROOT = BASE_DIR / "staticfiles"  # type: ignore[name-defined]
+
+TmsLogger("INFO")
