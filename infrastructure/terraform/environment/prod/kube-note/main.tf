d96c3850d192cb94d7f31222a553b5517c67dedc
Max Duessler <max.duessler@posteo.net>
2026-06-12 09:38:36 +0200

    refactor: Moved code for better code quality

--- infrastructure/terraform/environment/prod/kube-note/main.tf
+++ infrastructure/terraform/environment/prod/kube-note/main.tf
@@ -1,33 +1,3 @@
-module "controller" {
-  source             = "../../../modules/compute"
-  ami_owners         = ["self"]
-  ami_filter_values  = ["kube-node-*"]
-  instance_type      = var.instance_type
-  subnet_id          = module.network.private_subnet_id
-  security_groups    = [module.network.security_group_id]
-  permission_profile = module.security.instance_profile_name
-  http_hops          = 2
-  root_volume_size   = 20
-  namespace          = var.environment
-  name               = "kube-controller"
-}
-
-module "worker" {
-  for_each = var.workers
-
-  source             = "../../../modules/compute"
-  ami_owners         = ["self"]
-  ami_filter_values  = ["kube-node-*"]
-  instance_type      = var.instance_type
-  subnet_id          = module.network.private_subnet_id
-  security_groups    = [module.network.security_group_id]
-  permission_profile = module.security.instance_profile_name
-  http_hops          = 2
-  root_volume_size   = 50
-  namespace          = var.environment
-  name               = each.value
-}
-
 module "network" {
   source     = "../../../modules/network"
   aws_region = var.aws_region
@@ -106,4 +76,34 @@ resource "aws_security_group" "worker" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+module "controller" {
+  source             = "../../../modules/compute"
+  ami_owners         = ["self"]
+  ami_filter_values  = ["kube-node-*"]
+  instance_type      = var.instance_type
+  subnet_id          = module.network.private_subnet_id
+  security_groups    = [module.network.security_group_id]
+  permission_profile = module.security.instance_profile_name
+  http_hops          = 2
+  root_volume_size   = 20
+  namespace          = var.environment
+  name               = "kube-controller"
+}
+
+module "worker" {
+  for_each = var.workers
+
+  source             = "../../../modules/compute"
+  ami_owners         = ["self"]
+  ami_filter_values  = ["kube-node-*"]
+  instance_type      = var.instance_type
+  subnet_id          = module.network.private_subnet_id
+  security_groups    = [module.network.security_group_id]
+  permission_profile = module.security.instance_profile_name
+  http_hops          = 2
+  root_volume_size   = 50
+  namespace          = var.environment
+  name               = each.value
 }
