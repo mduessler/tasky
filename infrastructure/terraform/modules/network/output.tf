5cf0d3c37d70505ae51750e7efcbd1ea6844177c
Max Duessler <max.duessler@posteo.net>
2026-06-12 08:51:08 +0200

    feat: Added cidr block of the cidr

--- infrastructure/terraform/modules/network/output.tf
+++ infrastructure/terraform/modules/network/output.tf
@@ -3,6 +3,11 @@ output "vpc" {
   value       = aws_vpc.this.id
 }
 
+output "vpc_cidr" {
+  description = "CIDR of the vpc"
+  value       = aws_vpc.this.cidr_block
+}
+
 output "private_subnet" {
   description = "ID of the private subnet"
   value       = aws_subnet.private.id
