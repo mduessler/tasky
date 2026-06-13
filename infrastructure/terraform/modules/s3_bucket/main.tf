65877f2cf0906cd1d50cbaa0f136bd486f209791
Max Duessler <max.duessler@posteo.net>
2026-06-13 13:41:42 +0200

    chore: Formated layout

--- infrastructure/terraform/modules/s3_bucket/main.tf
+++ infrastructure/terraform/modules/s3_bucket/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "this" {
   bucket = var.name
-  tags = var.tags
+  tags   = var.tags
 
   lifecycle {
     prevent_destroy = true
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "this" {
 }
 
 resource "aws_s3_bucket_versioning" "this" {
-  count = var.version_status != null ? 1 : 0
+  count  = var.version_status != null ? 1 : 0
   bucket = aws_s3_bucket.this.id
   versioning_configuration {
     status = var.version_status
