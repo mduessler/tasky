c54bdf5b71103f2a20040326016b2229300c8b1b
Max Duessler <max.duessler@posteo.net>
2026-06-13 20:34:28 +0200

    fix: Foxed wrong id field

--- infrastructure/terraform/environment/dev/network/output.tf
+++ infrastructure/terraform/environment/dev/network/output.tf
@@ -1,4 +1,4 @@
 output "vpc_ids" {
   description = "List of all created vpcs for the development"
-  value       = { for az, net in module.network : az => net.vpc_id }
+  value       = { for az, net in module.network : az => net.vpc }
 }
