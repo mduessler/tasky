ec4ac756f28dbce7bc5521c3243ab8375172a3ea
Max Duessler <max.duessler@posteo.net>
2026-06-13 15:24:12 +0200

    chore: Formated code

--- infrastructure/terraform/modules/s3_bucket/main.tf
+++ infrastructure/terraform/modules/s3_bucket/main.tf
@@ -26,7 +26,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
 }
 
 resource "aws_s3_bucket_public_access_block" "this" {
-  bucket = aws_s3_bucket.this.id
+  bucket                  = aws_s3_bucket.this.id
   block_public_acls       = var.block_public_access
   block_public_policy     = var.block_public_access
   ignore_public_acls      = var.block_public_access
