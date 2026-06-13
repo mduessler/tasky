71f01a9dc5475897ad1c0bcd70b0f8ab25bfb0a9
Max Duessler <max.duessler@posteo.net>
2026-06-13 18:55:48 +0200

    feat: Added backend for network

--- infrastructure/terraform/environment/dev/network/provider.tf
+++ infrastructure/terraform/environment/dev/network/provider.tf
@@ -1,5 +1,10 @@
 terraform {
   required_version = ">= 1.15.3"
+  backend "s3" {
+    region       = "eu-central-1"
+    use_lockfile = true
+    encrypt      = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
