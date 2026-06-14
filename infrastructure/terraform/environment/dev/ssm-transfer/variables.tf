2387237bb7495f88fbf1181f876295de47ee04d2
Max Duessler <max.duessler@posteo.net>
2026-06-14 10:34:36 +0200

    fix: Added mission aws_region for bucket location

--- infrastructure/terraform/environment/dev/ssm-transfer/variables.tf
+++ infrastructure/terraform/environment/dev/ssm-transfer/variables.tf
@@ -3,6 +3,11 @@ variable "environment" {
   type        = string
 }
 
+variable "aws_region" {
+  description = "Deployment name of the environment."
+  type        = string
+}
+
 variable "owner_id" {
   description = "ID of the owner."
   type        = string
