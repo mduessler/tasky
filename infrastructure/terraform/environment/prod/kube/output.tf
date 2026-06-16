82c8e1f4999644a6993825936f8f9b96a63fe011
Max Duessler <max.duessler@posteo.net>
2026-06-16 09:29:19 +0200

    feat: Added output for kubes

--- infrastructure/terraform/environment/prod/kube/output.tf
+++ infrastructure/terraform/environment/prod/kube/output.tf
@@ -1,3 +1,6 @@
-output "postgres_volume_id" {
-  value = aws_ebs_volume.postgres.id
+output "kubes" {
+  description = "Set of all kubes grouped by there vpc id"
+  value = {
+    for kube in module.kubes : vpc_id => { conroller = kube.controller, workers = kube.workers }
+  }
 }
